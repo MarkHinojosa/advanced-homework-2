97f2891f0b4dbd354cebdc69f1865fafa3b7f642
Mark Hinojosa <markhinojosa1212@gmail.com>
2018-02-05 13:41:52 -0600

    completed and created new branch

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import state from "./state";
-
+// pulling state from state.js
 function render() {
   let currentCategory = "bumpers";
   
